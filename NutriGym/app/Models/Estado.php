6fa8bacfd3e024f754dd35789a76c5473df5d50b
McFurion <mirandabenja.q@gmail.com>
2025-10-11 09:13:55 -0500

    Agregar actualizaciones

---
+++ NutriGym/app/Models/Estado.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Estado extends Model
+{
+    //
+    use HasFactory;
+
+    protected $table = 'estado_fisico';
+
+    // Relación: relacion 1 --> todo usuario comienza con un estado y termina con otro
+    public function usuarios()
+    {
+        return $this->hasMany(Medida::class, 'id_estado');
+    }
+
+
+}
